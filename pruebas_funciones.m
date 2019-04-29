0b0c19b2e97fbb176301c92f3ce14927b5945799
Leticia <letigr29d@gmail.com>
2019-04-29 15:10:50 +0200

    aplicacion de matriz de cambio de base(error debido a retardo al comenzar a medir)

--- pruebas_funciones.m
+++ pruebas_funciones.m
@@ -1,16 +1,19 @@
 
 addpath('Funciones\')
-med_cal=cargar_datos_camara('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos optitrack\25-04-2019\Take 2019-04-25 12.19.51 PM_001.csv');
-med_cam=cargar_datos_camara('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos optitrack-shimmers\04-04-2019\datos_vicon_session4\Take 2019-04-04 12.13.10 PM.csv');
-med_imu=cargar_datos_shimmer('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos optitrack-shimmers\04-04-2019\datos_imu_Session4\default_exp_Session4_idBFED_Calibrated_SD.csv');
-med_cam.Rigid_Body.RigidBody.Rotation=[-med_cam.Rigid_Body.RigidBody.Rotation(:,4), med_cam.Rigid_Body.RigidBody.Rotation(:,1:3)];
+med_cal=cargar_datos_camara('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos optitrack\29-04-2019\Take 2019-04-29 11.50.13 AM.csv');
+med_cam=cargar_datos_camara('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos optitrack\29-04-2019\Take 2019-04-29 01.14.29 PM.csv');
+med_imu=cargar_datos_shimmer('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos shimmer\29-04-2019\orientacion_Session1_idBFED_Calibrated_SD.csv');
+ med_cam.Rigid_Body.RigidBody.Rotation=-[med_cam.Rigid_Body.RigidBody.Rotation(:,4), med_cam.Rigid_Body.RigidBody.Rotation(:,1:3)];
+%med_cam.Rigid_Body.RigidBody.Rotation=-med_cam.Rigid_Body.RigidBody.Rotation;
 %%
-plot_camara_imu_2D(med_cal,{med_imu});
-med_imu_s=sincronizar_imus(med_cam,{med_imu},1818);
-
+plot_camara_imu_2D(med_cam,{med_imu});
+%%
+med_imu_s=sincronizar_imus(med_cam,{med_imu},3083-1128);
+plot_camara_imu_2D(med_cam,med_imu_s);
 %%
 
 m_ali=matriz_ali(med_cal)
+
 %%
 
 plot_camara_imu_3D(med_cam,med_imu_s) 
@@ -19,7 +22,7 @@ plot_camara_imu_3D(med_cam,med_imu_s)
 
 % med_imu_s{1}.euler2quat=eul2quat(unwrap(quat2eul(med_imu_s{1}.Quat)));
 for t=1:length(med_imu_s{1}.Quat)
-    med_imu_s{1}.euler2quat(t,:)=dcm2quat(m_ali*quat2dcm(med_imu_s{1}.Quat(t,:))*[0 0 1; 0 1 0; -1 0 0]);
+    med_imu_s{1}.euler2quat(t,:)=([1 0 0 0 ;0 0 1 0 ;0 0 0 1;0 1 0 0]*(med_imu_s{1}.Quat(t,:)'));
 end
 
  [pitch, roll, yaw]=quat2angle( med_imu_s{1}.euler2quat,'XYZ');
