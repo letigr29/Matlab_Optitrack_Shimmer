05e59e5a8e0a3e2efd5c36f1c9417f3d31a80db2
Leticia <letigr29d@gmail.com>
2019-04-30 11:02:39 +0200

    matriz de cambio de base sin calibracion previa(error:no se puede calcular porque resulta en un sistema de ecuaciones de infinitas soluciones)

--- pruebas_funciones.m
+++ pruebas_funciones.m
@@ -1,16 +1,19 @@
 
 addpath('Funciones\')
-med_cal=cargar_datos_camara('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos optitrack\25-04-2019\Take 2019-04-25 12.19.51 PM_001.csv');
+% med_cal=cargar_datos_camara('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos optitrack\25-04-2019\Take 2019-04-25 12.19.51 PM_001.csv');
 med_cam=cargar_datos_camara('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos optitrack-shimmers\04-04-2019\datos_vicon_session4\Take 2019-04-04 12.13.10 PM.csv');
 med_imu=cargar_datos_shimmer('G:\Mi unidad\Universidad\Doctorado\Mediciones\Datos optitrack-shimmers\04-04-2019\datos_imu_Session4\default_exp_Session4_idBFED_Calibrated_SD.csv');
-med_cam.Rigid_Body.RigidBody.Rotation=[-med_cam.Rigid_Body.RigidBody.Rotation(:,4), med_cam.Rigid_Body.RigidBody.Rotation(:,1:3)];
+med_cam.Rigid_Body.RigidBody.Rotation=-[med_cam.Rigid_Body.RigidBody.Rotation(:,4), med_cam.Rigid_Body.RigidBody.Rotation(:,1:3)];
 %%
 plot_camara_imu_2D(med_cal,{med_imu});
 med_imu_s=sincronizar_imus(med_cam,{med_imu},1818);
 
 %%
 
-m_ali=matriz_ali(med_cal)
+[m_ali0,m_ali]=matriz_ali(med_imu_s,med_cam);
+m_ali0=real(m_ali0)
+m_ali=real(m_ali)
+
 %%
 
 plot_camara_imu_3D(med_cam,med_imu_s) 
@@ -19,7 +22,7 @@ plot_camara_imu_3D(med_cam,med_imu_s)
 
 % med_imu_s{1}.euler2quat=eul2quat(unwrap(quat2eul(med_imu_s{1}.Quat)));
 for t=1:length(med_imu_s{1}.Quat)
-    med_imu_s{1}.euler2quat(t,:)=dcm2quat(m_ali*quat2dcm(med_imu_s{1}.Quat(t,:))*[0 0 1; 0 1 0; -1 0 0]);
+    med_imu_s{1}.euler2quat(t,:)=(m_ali0*quat2dcm(med_imu_s{1}.Quat)*m_ali);
 end
 
  [pitch, roll, yaw]=quat2angle( med_imu_s{1}.euler2quat,'XYZ');
@@ -30,7 +33,7 @@ med_cam.Rigid_Body.RigidBody.angle=unwrap([pitch, roll, yaw]);
 % med_cam.Rigid_Body.RigidBody.euler2quat=eul2quat(unwrap(quat2eul( med_cam.Rigid_Body.RigidBody.Rotation)));
 
 for t=1:length(med_cam.Rigid_Body.RigidBody.Rotation)
-    med_cam.Rigid_Body.RigidBody.euler2quat(t,:)=dcm2quat(quat2dcm(med_cam.Rigid_Body.RigidBody.Rotation(t,:))*[0 0 1; 0 1 0; -1 0 0]);
+    med_cam.Rigid_Body.RigidBody.euler2quat(t,:)=(quat2dcm(med_cam.Rigid_Body.RigidBody.Rotation(t,:)));
 end
 %%
 plot_camara_imu_2D(med_cam,med_imu_s);
