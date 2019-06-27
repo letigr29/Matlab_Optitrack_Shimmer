ae6303d9e029dacd91d2c64a0f5b448ca12527e6
Leticia <letigr29d@gmail.com>
2019-06-27 12:32:27 +0200

    Mas errores en pruebas_funciones

--- Funciones/plot_camara_imu_3D.m
+++ Funciones/plot_camara_imu_3D.m
@@ -9,14 +9,14 @@
 %   sensores en el plot3D
 % ejes:= limites de los ejes
 
-function []= plot_camara_imu_3D(med_cam,med_imu,m_ali,forma,ejes)
+function []= plot_camara_imu_3D(med_cam,med_imu,forma,ejes)
 
     % argumentos por defecto
-    if (nargin<4)
+    if (nargin<3)
 		forma = struct('p1',[0,0,50],'p2',[0,0,-50],...
            'p3',[0,50,0]); 
        ejes=[-500 1200 -1000 600 -500 1000];
-	elseif (nargin<5)
+	elseif (nargin<4)
        ejes=[-500 1200 -1000 600 -500 1000];
     end
     
@@ -31,14 +31,13 @@ function []= plot_camara_imu_3D(med_cam,med_imu,m_ali,forma,ejes)
    
     rigid_bodies = fieldnames(med_cam.Rigid_Body);
     rigid_body_markers=fieldnames(med_cam.Rigid_Body_Marker);
-    colores={'r','b','g','m','c','y','r','b','g','m','c','y','r','b','g','m','c','y'};
     
     items_3D={nombres_imus{:},rigid_bodies{:}};%cell(1,length(nombres_imus)+length(rigid_bodies))};
     items_3D_selec=zeros(1,length(nombres_imus)+length(rigid_bodies));
     
     % controles
     f=figure('Position',[200 100 1000 800]);
-    ax_3D=axes('units','pixels','position',[100 100 500 700]);
+    ax_3D=axes('units','pixels','position',[100 100 700 800]);
     time_control = uicontrol('Parent',f,'Style','slider','Position',[50,50,900,25],...
               'value',1, 'min',1, 'max',length(med_cam.tiempo)-25);
        
@@ -62,7 +61,10 @@ function []= plot_camara_imu_3D(med_cam,med_imu,m_ali,forma,ejes)
               'String','Pausa','value',1);
 
   
-     
+    colores={'r','b','g','m'};
+    estilos={'-','--',':','-.'};
+    plot_1=[];
+    plot_2=[];
     % bucle principal
     
     while true
@@ -70,68 +72,122 @@ function []= plot_camara_imu_3D(med_cam,med_imu,m_ali,forma,ejes)
         t=round(time_control.Value);
         multi=b2.Value;
         
+        color =1;
         if(b3.Value)
             hold(ax_3D, 'off');
             for rb=rigid_bodies'
                 if  items_3D_selec(strcmp(rb,items_3D))
-                quaternion= med_cam.Rigid_Body.(rb{1}).Rotation(t,:) ;
-         
-                forma_rotada.p1 = [1 0 0; 0 0 -1; 0 1 0]*quatrotate(quaternion, forma.p1)';
-                forma_rotada.p2 = [1 0 0; 0 0 -1; 0 1 0]*quatrotate(quaternion, forma.p2)';
-                forma_rotada.p3 = [1 0 0; 0 0 -1; 0 1 0]*quatrotate(quaternion, forma.p3)';
+                    quaternion= med_cam.Rigid_Body.(rb{1}).Rotation(t,:) ;
 
+                    posicion= [1 0 0; 0 0 -1; 0 1 0]*med_cam.Rigid_Body.(rb{1}).Position(t,:)';% cambio de base entre sistema de coordenadas de las camaras al de matlab
+                    fields = fieldnames(forma);
 
-                posicion= [1 0 0; 0 0 -1; 0 1 0]*med_cam.Rigid_Body.(rb{1}).Position(t,:)';% cambio de base entre sistema de coordenadas de las camaras al de matlab
- 
-                plot3(ax_3D,posicion(1)+[forma_rotada.p1(1), forma_rotada.p2(1)],...
-                    posicion(2)+[forma_rotada.p1(2), forma_rotada.p2(2)],...
-                    posicion(3)+[forma_rotada.p1(3),forma_rotada.p2(3)],'-k','LineWidth',2)
-                hold(ax_3D, 'on');
-                plot3(ax_3D,posicion(1)+[forma_rotada.p2(1), forma_rotada.p3(1)],...
-                    posicion(2)+[forma_rotada.p2(2), forma_rotada.p3(2)],...
-                    posicion(3)+[forma_rotada.p2(3),forma_rotada.p3(3)],'-k','LineWidth',2)
-                plot3(ax_3D,posicion(1)+[forma_rotada.p3(1), forma_rotada.p1(1)],...
-                    posicion(2)+[forma_rotada.p3(2), forma_rotada.p1(2)],...
-                   posicion(3)+[forma_rotada.p3(3),forma_rotada.p1(3)],'-k','LineWidth',2)
-                
-                xlabel(ax_3D,'X')
-                ylabel(ax_3D,'Y')
-                zlabel(ax_3D,'Z')
-                grid(ax_3D,'on')
-                axis(ax_3D,'equal',ejes)
+                    for f=1:length(fields)
+                         forma_rotada.(fields{f})=[1 0 0; 0 0 -1; 0 1 0]*quatrotate(quaternion, forma.(fields{f}))';
+
+                    end
+
+                    % Representacion
+
+                    p1=forma_rotada.(fields{1});
+                        p2=forma_rotada.(fields{2});
+
+                    plot_1=plot3(ax_3D,posicion(1)+[p1(1), p2(1)],...
+                          posicion(2)+[p1(2),p2(2)],...
+                          posicion(3)+[p1(3),p2(3)],'-k','LineWidth',2,'Color',colores{color})
+                    hold on;
+
+                    for f=2:length(fields)-1
+
+                        p1=forma_rotada.(fields{f});
+                        p2=forma_rotada.(fields{f+1});
+
+                        plot3(ax_3D,posicion(1)+[p1(1), p2(1)],...
+                          posicion(2)+[p1(2),p2(2)],...
+                          posicion(3)+[p1(3),p2(3)],'-k','LineWidth',2,'Color',colores{color})
+
+                    end
+
+                    p1=forma_rotada.(fields{end});
+                    p2=forma_rotada.(fields{1});
+
+                    plot3(ax_3D,posicion(1)+[p1(1), p2(1)],...
+                      posicion(2)+[p1(2),p2(2)],...
+                      posicion(3)+[p1(3),p2(3)],'-k','LineWidth',2,'Color',colores{color})
+
+                    xlabel(ax_3D,'X[mm]')
+                    ylabel(ax_3D,'Y[mm]')
+                    zlabel(ax_3D,'Z[mm]')
+                    grid(ax_3D,'on')
+                    axis(ax_3D,'equal',ejes)
+                    title('Posición 3D')
+                   
+                    if color==length(colores)
+                       color=0; 
+                    end
+                    color=color+1;
 
                 end
+                
+              
             end
             
              for i=1:num_imus 
                 if  items_3D_selec(strcmp( nombres_imus{i},items_3D))
                     if ~isnan(med_imu{strcmp( nombres_imus{i},items_3D)}.Quat)
-                      
-                        quaternion=med_imu{strcmp( nombres_imus{i},items_3D)}.euler2quat(t,:) ;
+                
 
-                        forma_rotada.p1 = [1 0 0; 0 0 -1; 0 1 0]*quatrotate(quaternion, forma.p1)';
-                        forma_rotada.p2 = [1 0 0; 0 0 -1; 0 1 0]*quatrotate(quaternion, forma.p2)';
-                        forma_rotada.p3 = [1 0 0; 0 0 -1; 0 1 0]*quatrotate(quaternion, forma.p3)';
+                        quaternion=med_imu{i}.Rotation(t,:);
 
-                        posicion=[1 0 0; 0 0 -1; 0 1 0]* med_cam.Rigid_Body.(rb{1}).Position(t,:)';% cambio de base entre sistema de coordenadas de las camaras al de matlab
+                        posicion= [1 0 0; 0 0 -1; 0 1 0]*med_cam.Rigid_Body.(rb{1}).Position(t,:)';% cambio de base entre sistema de coordenadas de las camaras al de matlab
+                        fields = fieldnames(forma);
 
-                        plot3(ax_3D,posicion(1)+[forma_rotada.p1(1), forma_rotada.p2(1)],...
-                            posicion(2)+[forma_rotada.p1(2), forma_rotada.p2(2)],...
-                            posicion(3)+[forma_rotada.p1(3),forma_rotada.p2(3)],'-k','LineWidth',2)
-                        hold(ax_3D, 'on');
-                        plot3(ax_3D,posicion(1)+[forma_rotada.p2(1), forma_rotada.p3(1)],...
-                            posicion(2)+[forma_rotada.p2(2), forma_rotada.p3(2)],...
-                            posicion(3)+[forma_rotada.p2(3),forma_rotada.p3(3)],'-k','LineWidth',2)
-                        plot3(ax_3D,posicion(1)+[forma_rotada.p3(1), forma_rotada.p1(1)],...
-                            posicion(2)+[forma_rotada.p3(2), forma_rotada.p1(2)],...
-                           posicion(3)+[forma_rotada.p3(3),forma_rotada.p1(3)],'-k','LineWidth',2)
-                       
-                        xlabel(ax_3D,'X')
-                        ylabel(ax_3D,'Y')
-                        zlabel(ax_3D,'Z')
+                        for f=1:length(fields)
+                             forma_rotada.(fields{f})=[1 0 0; 0 0 -1; 0 1 0]*quatrotate(quaternion, forma.(fields{f}))';
+
+                        end
+
+                        
+                        
+                       % Representacion
+        
+                        p1=forma_rotada.(fields{1});
+                            p2=forma_rotada.(fields{2});
+
+                       plot_2=plot3(ax_3D,posicion(1)+[p1(1), p2(1)],...
+                              posicion(2)+[p1(2),p2(2)],...
+                              posicion(3)+[p1(3),p2(3)],'-k','LineWidth',2,'Color',colores{color})
+                        hold on;
+
+                        for f=2:length(fields)-1
+
+                            p1=forma_rotada.(fields{f});
+                            p2=forma_rotada.(fields{f+1});
+
+                            plot3(ax_3D,posicion(1)+[p1(1), p2(1)],...
+                              posicion(2)+[p1(2),p2(2)],...
+                              posicion(3)+[p1(3),p2(3)],'-k','LineWidth',2,'Color',colores{color})
+
+                        end
+
+                        p1=forma_rotada.(fields{end});
+                        p2=forma_rotada.(fields{1});
+
+                        plot3(ax_3D,posicion(1)+[p1(1), p2(1)],...
+                          posicion(2)+[p1(2),p2(2)],...
+                          posicion(3)+[p1(3),p2(3)],'-k','LineWidth',2,'Color',colores{color})
+
+
+                        xlabel(ax_3D,'X[mm]')
+                        ylabel(ax_3D,'Y[mm]')
+                        zlabel(ax_3D,'Z[mm]')
                         grid(ax_3D,'on')
                         axis(ax_3D,'equal',ejes)
                 
+                        if color==length(colores)
+                           color=0; 
+                        end
+                        color=color+1;
                     end
                 end
              end
@@ -143,6 +199,8 @@ function []= plot_camara_imu_3D(med_cam,med_imu,m_ali,forma,ejes)
             end
             
         end
+        
+        legend([plot_1,plot_2],items_3D(find(items_3D_selec==1))) %%%%%%%%%%%%%%%%%%%
         drawnow
     end
 
--- Funciones/transformacion_cuaterniones.m
+++ Funciones/transformacion_cuaterniones.m
@@ -7,10 +7,10 @@
 %
 % quat_final = mcb1*quat*inv(mcb0)
 
-function [m_imu]= transformacion_cuaterniones(m_imu,mcb0,mcb1)
+function [Rotation]= transformacion_cuaterniones(Quat,mcb0,mcb1)
     
-    for t=1:length(m_imu.Quat)
-        m_imu.Rotation(t,:)=(dcm2quat((mcb1)*quat2dcm(m_imu.Quat(t,:))*inv(mcb0)));
+    for t=1:length(Quat)
+        Rotation(t,:)=(dcm2quat((mcb1)*quat2dcm(Quat(t,:))*inv(mcb0)));
     end
     
 end
